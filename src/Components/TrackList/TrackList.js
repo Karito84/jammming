f963741e2d89bb111c6d452dfac80e153cda8f68
KAROLINA_MUELLER <karolina.gawlowska@gmail.com>
2022-08-15 20:54:57 -0400

    pass isRemoval and onRemove to Track

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -12,6 +12,8 @@ class TrackList extends React.Component {
                         return <Track track={track} 
                                     key={track.id }
                                     onAdd={this.props.onAdd}
+                                    isRemoval={this.props.isRemoval}
+                                    onRemove={this.props.onRemove}
                         /> 
                     })
                 }

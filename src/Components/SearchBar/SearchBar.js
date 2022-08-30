e6e90f6d9fb40209a2d2655e09a48da7011820af
KAROLINA_MUELLER <karolina.gawlowska@gmail.com>
2022-08-29 20:49:33 -0400

    Add state abd onClick to bttn in SearchBar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -4,26 +4,30 @@ import './SearchBar.css'
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-
+    
+    this.state = {
+      searchTerm: ''
+    };
     this.search = this.search.bind(this);
     this.handleTermChange = this.handleTermChange.bind(this);
   }
-    render() {
-        return (
-        <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist"
-                onChange={this.handleTermChange} />
-        <button className="SearchButton">SEARCH</button>
-      </div>)
-    }
-
-    search() {
+   
+  search() {
       this.props.onSearch(this.state.searchTerm);
     }
 
-    handleTermChange(e) {
+  handleTermChange(e) {
       this.setState({searchTerm: e.target.value});
     }
+
+  render() {
+      return (
+        <div className="SearchBar">
+          <input placeholder="Enter A Song, Album, or Artist"
+              onChange={this.handleTermChange} />
+          <button onClick={this.search} className="SearchButton">SEARCH</button>
+        </div>)
+      }
 }
 
 export default SearchBar;

6dc03a813e4b20cfac8f05e59388f8c660cc847b
KAROLINA_MUELLER <karolina.gawlowska@gmail.com>
2022-09-13 21:39:33 -0400

    add  onClick and handleClick()

--- src/Components/PlaylistListItem/PlaylistListItem.js
+++ src/Components/PlaylistListItem/PlaylistListItem.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./PlaylistListItem.css"
 
 class PlaylistListItem extends React.Component{
-    
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
 
+    handleClick() {
+        this.props.selectPlaylist(this.props.id);
+    }
     render() {
         return (
-        <div className="ListItem-information"> 
+        <div className="ListItem-information" onClick={this.handleClick}> 
             <p>{this.props.playlistName}</p>
 
         </div>
